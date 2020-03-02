9b177be3d390d792c914c431a435ec4e4634e763
Ramadhan Ilham <45709708+rmdhnilham@users.noreply.github.com>
2020-03-02 17:43:19 +0700

    tambah pesan

--- tugas2/udp_simple.py
+++ tugas2/udp_simple.py
@@ -4,5 +4,6 @@ import time
 TARGET_IP = "127.0.0.1"
 TARGET_PORT = 5006
 
+pesan = "Ini pesan"
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(bytes(65), (TARGET_IP, TARGET_PORT))
+sock.sendto(bytes(pesan.encode()),(TARGET_IP, TARGET_PORT))
